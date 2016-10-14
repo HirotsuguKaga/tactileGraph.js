593c1666f11b9b88b80d2cd55c1bc1fcc316d272
Hirotsugu Kaga <hkaga@eng.niigata-u.ac.jp>
2016-10-14 17:11:41 +0900

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,9 @@
+var tg = tactileGraphic();
+tg.setCanvas('a');
+
+tg.drawBraille("ABCDEFGmopyuihnkcrtz",1,1);
+tg.drawLine(40, 40, 100, 200);
+tg.drawLine(40, 40, 100, 500);
+tg.drawLine(40, 40, 500, 200);
+tg.drawLine(40, 40, 100, 300);
+
