3e005ca69721f01f14a33a9e88a893418f252909
Hirotsugu Kaga <hkaga@eng.niigata-u.ac.jp>
2016-12-14 15:22:17 +0900

    added line

--- calender.js
+++ calender.js
@@ -5,7 +5,7 @@ function drawGraph(){///////////////////////////////////////////////////////
   tg.clear();
   tg.setSize("B5");
 
-  // 下準備 //
+  // カレンダーの準備 //
   myDate = new Date();	// 今日の日付データ取得
   myWeekTbl = new Array("にち","げつ","か","すい","もく","きん","ど");	// 曜日テーブル
   myMonthTbl= new Array(31,28,31,30,31,30,31,31,30,31,30,31);	// 月テーブル
@@ -22,57 +22,24 @@ function drawGraph(){///////////////////////////////////////////////////////
 
   for(i=0; i<7*myTblLine; i++) myTable[i]="_";	// myTableを掃除
   for(i=0; i<myMonthTbl[myMonth]; i++)myTable[i+myWeek]=i+1;	// 日付を埋め込む
-
-console.log(myTable);
- 	
-// ***********************	
-//      カレンダーの表示	
-// ***********************	
-
-var month = myMonth + 1 ;
-tg.drawBraille(myYear + "ねん　" + month + "がつ"); 
-
-for(var i=0; i<7; i++){
-  tg.drawBraille(myWeekTbl[i],i*60,80); 
-}
-
-for(i=0; i<myTblLine; i++){
-  for(j=0; j<7; j++){
-    var myDat = myTable[j+(i*7)];
-    console.log(myDat);
-    tg.drawBraille(myDat,j*60,130+i*50);
+ //      カレンダーの表示	
+  var month = myMonth + 1 ;
+  tg.drawBraille(myYear + "ねん　" + month + "がつ"); 
+  tg.drawLine(0, 110, 420, 110);
+  for(var i=0; i<7; i++){
+    tg.drawBraille(myWeekTbl[i],i*60,80); 
+  }
+  for(i=0; i<myTblLine; i++){
+    for(j=0; j<7; j++){
+      var myDat = myTable[j+(i*7)];
+      console.log(myDat);
+      tg.drawBraille(myDat,j*60,130+i*50);
+    }
   }
 }
-
-//document.write("<strong>",myYear, "年", (myMonth+1), "月カレンダー</strong>");	
-//document.write("</td></tr>");	
-//document.write("<tr>");	// 曜日見出しセット
-//for(i=0; i<7; i++){	// 一行(１週間)ループ
-//  document.write("<td align='center' ");	
-//  if(i==0)document.write("bgcolor='#fa8072'>");	// 日曜のセルの色
-//  else document.write("bgcolor='#ffebcd'>");	// 月～土のセルの色
-//  document.write("<strong>",myWeekTbl[i],"</strong>");	// '日'から'土'の表示
-//  document.write("</td>");	
-//}	
-//document.write("</tr>");	
- 	
-//for(i=0; i<myTblLine; i++){	// 表の「行」のループ
-//document.write("<tr>");	// 行の開始
-//for(j=0; j<7; j++){	// 表の「列」のループ
-// document.write("<td align='center' ");	// 列(セル)の作成
-// myDat = myTable[j+(i*7)];	// 書きこむ内容の取得
-// if (myDat==myToday)document.write("bgcolor='#00ffff'>");	// 今日のセルの色
-// else if(j==0) document.write("bgcolor='#ffb6c1'>");	// 日曜のセルの色
-// else document.write("bgcolor='#ffffe0'>");	// 平日のセルの色
-// document.write("<strong>",myDat,"</strong>");	// 日付セット
-//}
-
-}
-               ///////////download///////////
+          ///////////Download///////////
 var filename = "Graph";
-
 var edl = document.querySelector('#edl');
-//var png = document.querySelector('#png');
 var esa = document.querySelector('#esa');
 
 edl.onclick = function() {
